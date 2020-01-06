3f9fa94e55c32cf8db24af9aab3c0141169a5ca4
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-01-06 14:43:30 -0800

    save button saves textarea value to todoList array

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 
-// an array of time in the work day
+// array of time in the work day
 let times = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
-let todoList = [];
-//let toDoTextArea;
+let todoList = ["", "", "", "", "", "", "", "", ""];
+let toDoText;
 
 // create a row for each time of the day that includes a textarea and a save button
 $.each(times, function(index, time) {
     let timeRow = $('<div class="row">')
     let hour = $('<div class="col-md-1 hour">').text(time);
-    hour.attr("data-time", time);
     timeRow.append(hour);
-    toDoTextArea = $('<textarea class="col-md-10 past"></textarea>');
-    //toDoTextArea.attr("data-todo", index);
-    timeRow.append(toDoTextArea);
-
-    saveButton = $('<button class="col-md-1 saveBtn index">Save</button>');
-    saveButton.attr("data-todo", index);
-    //console.log(saveButton.attr("data-todo"));
+    toDoText = $('<textarea class="col-md-10 past"></textarea>');
+    toDoText.attr("id", index);
+    //toDoText.val(todoList[index]);
+    timeRow.append(toDoText);
+    saveButton = $('<button class="col-md-1 saveBtn">Save</button>');
+    saveButton.attr("data-save", index);
     timeRow.append(saveButton);
     $('.container').append(timeRow);
 })
 
 $('.saveBtn').on('click', function() {
-    let index = $(this).attr("data-todo");
-    console.log(index);
+    let index = $(this).attr("data-save");
+    let textIndex = '#' + index;
+    todoList[index] = $(textIndex).val();
+
+    console.log(todoList);
+    //console.log($(this).previousElementSibling);
 })
