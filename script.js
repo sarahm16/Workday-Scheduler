e072a3b65c4ce349e7e479c629d9a46be393e164
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-01-09 13:30:56 -0800

    set local storage to empty array if it hasn't been set yet. Added JSON.stringify to the empty array

--- script.js
+++ script.js
@@ -2,40 +2,60 @@
 // array of time in the work day
 let times = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-let todoList = ["", "", "", "", "", "", "", "", ""];
 let toDoText;
+let todoList = ["","","","","","","","",""];
 
+// sets local storage if it hasn't been set yet
+if(localStorage.getItem("todo-list") == undefined) {
+    localStorage.setItem("todo-list", JSON.stringify(todoList));
+}
+
+// create a moment object to retrieve current hour
 let m = moment();
-let x = m.hour();
+let currentHour = m.hour();
 
-// create a row for each time of the day that includes a textarea and a save button
+// create a row for each time of the work day
 $.each(hours, function(index, time) {
+
+    // create new time block
     let timeRow = $('<div class="row time-block">')
+
+    //create and append time to time block
     let hour = $('<div class="col-md-1 hour">').text(times[index]);
     timeRow.append(hour);
+
+    // create and append text area
     toDoText = $('<textarea></textarea>');
     timeRow.append(toDoText);
     toDoText.attr("id", index);
-    if(x > hours[index]) {
+
+    // change colors of past, present, and future time blocks
+    if(currentHour > hours[index]) {
         toDoText.attr("class", "past col-md-10");
     }
-    else if(x == hours[index]) {
+    else if(currentHour == hours[index]) {
         toDoText.attr("class", "present col-md-10");
     }
     else {
         toDoText.attr("class", "future col-md-10");
     }
-    //toDoText.val(todoList[index]);
+
+    // retrieve todoList from local storage and set value of text area
+    todoList = JSON.parse(localStorage.getItem("todo-list"));
+    toDoText.val(todoList[index]);
+
+    // create and append save button
     saveButton = $('<button class="col-md-1 saveBtn">Save</button>');
     saveButton.attr("data-save", index);
     timeRow.append(saveButton);
     $('.container').append(timeRow);
 })
 
+// add event listener to each save button, store value of text area into local storage
 $('.saveBtn').on('click', function() {
     let index = $(this).attr("data-save");
     let textIndex = '#' + index;
     todoList[index] = $(textIndex).val();
-    localStorage.setItem("todo", todoList);
-    console.log(todoList);
+    localStorage.setItem("todo-list", JSON.stringify(todoList));
+
 })
